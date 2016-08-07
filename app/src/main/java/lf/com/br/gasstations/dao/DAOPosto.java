c41c0424a313a3647b5a4ba97273ccb353269283
Fernando Santos <lf.fernandosantos@gmail.com>
2016-08-07 09:07:32 -0300

    versão 3.0

---
+++ app/src/main/java/lf/com/br/gasstations/dao/DAOPosto.java
@@ -0,0 +1,7 @@
+package lf.com.br.gasstations.dao;
+
+/**
+ * Created by Fernando on 23/05/2016.
+ */
+public class DAOPosto {
+}
---
+++ app/src/main/java/lf/com/br/gasstations/model/DadosPosto.java
@@ -0,0 +1,26 @@
+package lf.com.br.gasstations.model;
+
+/**
+ * Created by Fernando on 23/05/2016.
+ */
+public class DadosPosto {
+
+    private String id;
+    private String coments;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getComents() {
+        return coments;
+    }
+
+    public void setComents(String coments) {
+        this.coments = coments;
+    }
+}
